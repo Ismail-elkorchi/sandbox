7726a1f425d4812c188b7c46fce76901e0b37de2
Ismail El Korchi <ismail.elkorchi@gmail.com>
2026-09-21 00:43:17 +0000

    Retarget guest protocol fuzzing to Sandsurf

--- fuzz/fuzz_targets/guest_protocol.rs
+++ fuzz/fuzz_targets/guest_protocol.rs
@@ -1,12 +1,12 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use sandbox_guest::{GuestRequest, GuestResponse};
+use sandsurf_protocol::{GuestServiceRequest, GuestServiceResponse, MAX_CONTROL_BYTES};
 
 fuzz_target!(|data: &[u8]| {
-    if data.len() > sandbox_guest::MAX_GUEST_FRAME {
+    if data.len() > MAX_CONTROL_BYTES {
         return;
     }
-    let _ = serde_json::from_slice::<GuestRequest>(data);
-    let _ = serde_json::from_slice::<GuestResponse>(data);
+    let _ = serde_json::from_slice::<GuestServiceRequest>(data);
+    let _ = serde_json::from_slice::<GuestServiceResponse>(data);
 });
